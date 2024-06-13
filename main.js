43e032ae0e436798bb94e672048ee70b249614ca
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-06-13 10:46:28 +0200

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Without middleware
+app.get("/", function (req, res) {
+  res.redirect("https://github.com/opral/monorepo/tree/main/lix");
+});
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
